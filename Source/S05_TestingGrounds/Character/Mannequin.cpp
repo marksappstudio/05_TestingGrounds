9a23d671ca8356d6afa404ab7673637e31df71c5
marksappstudio <marksappstudio>
2018-09-25 16:28:20 +1000

    S5, Lecture 310 - Procedural Level Generation

--- Source/S05_TestingGrounds/Character/Mannequin.cpp
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -34,15 +34,28 @@ AMannequin::AMannequin()
 void AMannequin::BeginPlay()
 {
     Super::BeginPlay();
-    if (GunBlueprint == NULL) {
+    if (GunBlueprint == nullptr) {
         UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
         return;
     }
     Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
-    Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
-    Gun->AnimInstance = GetMesh()->GetAnimInstance();
     
-    if (InputComponent !=NULL) {
+    //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+    if (IsPlayerControlled()) {
+        Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+        
+    } else {
+         Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint_0"));
+        
+    }
+    
+    Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+    
+    
+    Gun->AnimInstance1P = Mesh1P->GetAnimInstance();
+    Gun->AnimInstance3P = GetMesh()->GetAnimInstance();
+    
+    if (InputComponent !=nullptr) {
     InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
         
     }
@@ -61,6 +74,20 @@ void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
     Super::SetupPlayerInputComponent(PlayerInputComponent);
     
 }
+
+void AMannequin::UnPossessed()
+{
+    Super::UnPossessed();
+    
+    if (!(Gun))
+    {
+        UE_LOG(LogTemp, Error, TEXT("Gun is not available"))
+        return;
+    }
+    UE_LOG(LogTemp, Warning, TEXT("Gun re-attached"))
+    Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+}
+
 
 void AMannequin::PullTrigger()
 {
