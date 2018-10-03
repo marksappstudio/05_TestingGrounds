63ac1c11668bfe8a04bebccf3f766f3f1ffca7f4
marksappstudio <marksappstudio>
2018-10-03 19:03:40 +1000

    S5, Lecture 318 - Choosing Waypoint with EQS

--- Source/S05_TestingGrounds/Terrain/GrassComponent.cpp
+++ Source/S05_TestingGrounds/Terrain/GrassComponent.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "S05_TestingGrounds.h"
 #include "GrassComponent.h"
+#include "S05_TestingGrounds.h"
+
 
 
 // Sets default values for this component's properties
